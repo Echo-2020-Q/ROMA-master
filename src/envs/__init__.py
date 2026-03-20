cdf051d87bb2bd2d80d4c81678dd51daa23397f1
Echo-2020-Q <73975207+Echo-2020-Q@users.noreply.github.com>
2026-03-20 18:11:18 +0800

    update

---
+++ src/envs/__init__.py
@@ -0,0 +1,24 @@
+from functools import partial
+#from smac.env import MultiAgentEnv, StarCraft2Env
+from .multiagentenv import MultiAgentEnv
+from .starcraft2 import StarCraft2Env
+
+import sys
+import os
+
+try:
+    from .gfootball import GoogleFootballEnv
+except Exception:
+    GoogleFootballEnv = None
+
+def env_fn(env, **kwargs) -> MultiAgentEnv:
+    return env(**kwargs)
+
+REGISTRY = {}
+REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
+if GoogleFootballEnv is not None:
+    REGISTRY["gf"] = partial(env_fn, env=GoogleFootballEnv)
+
+if sys.platform == "linux":
+    os.environ.setdefault("SC2PATH",
+                          os.path.join(os.getcwd(), "3rdparty", "StarCraftII"))
